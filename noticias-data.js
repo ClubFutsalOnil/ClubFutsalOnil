b04962f26ca21630d302ddeb73827ef1e24d50d6
EstebanCamposArts <Estebancampospolo@gmail.com>
2025-06-13 15:43:05 +0200

    1

---
+++ noticias-data.js
@@ -0,0 +1,34 @@
+const noticias = [
+  {
+    titulo: "¡Arranca el Campus de Verano 2025 del Club de Futsal Onil!",
+    imagen: "assets/images/news/campusverano25.jpg",
+    contenido: "Ya puedes apuntarte al campus de fútbol sala del verano 2025. Entrenamientos, juegos, piscina y mucha diversión para niños y niñas de 6 a 16 años. Plazas limitadas. El Club de Futsal Onil ya tiene todo listo para su esperado Campus de Verano 2025, una oportunidad perfecta para que niños y jóvenes mejoren sus habilidades en el futsal durante las vacaciones. Este año, todos los participantes recibirán una mochila y una camiseta exclusiva del club, un regalo especial para acompañar su entrenamiento y fomentar el espíritu de equipo. El campus contará con entrenamientos técnicos, actividades recreativas y talleres de valores deportivos, todo dirigido por nuestros entrenadores expertos. ¡No te pierdas esta experiencia única y únete al mejor verano con el CFS Onil!",
+  },
+  {
+    titulo: "Prebenjamines jugarán un triangular con el Serelles de Alcoy.",
+    imagen: "assets/images/news/amistoso.jpg",
+    contenido: `
+      Los partidos serán de 30 minutos. <br>
+      Serelles - CFS Onil Prebenjamín A 10:00 h <br>
+      Serelles - CFS Onil Prebenjamín B 10:30 h <br>
+      CFS Onil Querubines - San Juan y San Pablo 11:00 h <br>
+      CFS Onil Prebenjamín A - CFS Onil Prebenjamín B 11:45 h <br>
+      CFS Onil Alevín - Futsal Ibi 12:30 h.
+    `
+  },
+  {
+    titulo: "Nuevas equipaciones para la temporada 2024-2025",
+    imagen: "assets/images/news/equipacion.jpg",
+    contenido: "Presentamos el diseño oficial de las nuevas camisetas del club para esta temporada. Modernas, funcionales y con los colores de siempre.",
+  },
+  {
+    titulo: "Hazte socio del CFS Onil por solo 10 € y apoya el deporte local.",
+    imagen: "assets/images/news/carnetsocio.png",
+    contenido: "El Club de Futsal Onil lanza su campaña de socios 2025 con una propuesta accesible para todos los aficionados.  El Club de Futsal Onil lanza su campaña de socios 2025 con una propuesta accesible para todos: por solo 10 €, puedes formar parte de la familia del club y apoyar el crecimiento del futsal en nuestra localidad. Con tu colaboración ayudas a que niños y jóvenes sigan disfrutando del deporte en un entorno sano, formativo y lleno de valores. Ser socio es una forma sencilla de apoyar este proyecto gestionado por padres voluntarios, sin ánimo de lucro. ¡Únete hoy y forma parte del equipo que hace posible el futsal en Onil!.",
+  },
+  {
+    titulo: "El sénior del CFS Onil ya conoce a sus rivales en La Preferente 2025",
+    imagen: "assets/images/news/temporadasenior.jpg",
+    contenido: "Nuestro equipo sénior disputará esta temporada la emocionante liga La Preferente. ¡A por todas! .  Nuestro equipo sénior disputará esta temporada la emocionante liga La Preferente, donde se medirá a clubes de gran nivel como CFS Castalla, Xaloc Alacant, Racing Novelda, CFS Elche Dream y muchos más. Será una competición exigente, pero llena de ilusión y trabajo en equipo. Además, contamos con el apoyo de nuestro patrocinador oficial TORRIKO, que confía en el proyecto y nos acompaña en esta nueva etapa. ¡Estamos listos para darlo todo en la pista y defender los colores del CFS Onil con orgullo!.",
+  }
+];
